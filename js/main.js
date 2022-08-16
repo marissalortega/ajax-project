f60d8da945a5cf6a36a285db28e46d6f9d5fde11
Marissa Ortega <marissaljames@gmail.com>
2022-08-16 04:40:16 +0000

    finished desktop and mobile layout for feature #1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
+// hamburger functionality
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
+  const weatherWidget = document.querySelector('.weather');
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
-
+    weatherWidget.classList.toggle('nav-active');
     burger.classList.toggle('toggle');
   });
 };
 
 navSlide();
+/*
+// accuweather API
+const weatherHigh = document.querySelector('.weather-high');
+const weatherLow = document.querySelector('.weather-low');
+
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/38824_PC');
+
+xhr.responseType = 'json';
+
+xhr.addEventListener('load', () => {
+  console.log(xhr.status);
+  console.log(xhr.response);
+});
+*/
